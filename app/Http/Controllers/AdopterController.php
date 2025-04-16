4f634eb8dfc24fd19ccb3b4d01963ffc975d6ecf
yaowenkhor <yaowenkhor@gmail.com>
2025-04-16 23:45:51 +0800

    Delete unused code

--- app/Http/Controllers/AdopterController.php
+++ app/Http/Controllers/AdopterController.php
@@ -5,8 +5,6 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\AdoptionApplication;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\DB;
-use App\Models\User;
 use App\Models\Pet;
 use Storage;
 
--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -11,7 +11,6 @@ use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Http\Request;
-use PhpParser\Node\Stmt\TryCatch;
 
 class RegisterController extends Controller
 {
